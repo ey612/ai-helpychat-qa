922dafc2f6602a6a24bc8961dfe52407b76d3ae3
[QA3]송정훈 <gold6426@naver.com>
2025-12-16 04:52:18 +0000

    Delete constants.py

--- junghoon/constants.py
+++
@@ -1,8 +0,0 @@
-AI_COMPLETE = 'div.elice-aichat__markdown[data-status="complete"]'
-
-CHAT_INPUT = "textarea[name='input']"
-REGENERATE_BTN = "button[aria-label='다시 생성']"
-EDIT_BTN = "button[aria-label='수정']"
-
-PREV_BTN = 'button:has(svg[data-testid="chevron-leftIcon"])'
-NEXT_BTN = 'button:has(svg[data-testid="chevron-rightIcon"])'
