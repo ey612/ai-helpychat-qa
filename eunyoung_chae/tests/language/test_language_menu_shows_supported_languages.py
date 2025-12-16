b9f833118ca9c5885da1c659494099fe66df8b7e
eunyoung chae <wooon840824@gmail.com>
2025-12-16 15:59:26 +0900

    세션 전체에서 로그인 한 번만 하도록 드라이버 수정 시도 = 실패

--- eunyoung_chae/tests/language/test_language_menu_shows_supported_languages.py
+++ eunyoung_chae/tests/language/test_language_menu_shows_supported_languages.py
@@ -9,21 +9,21 @@ from selenium.common.exceptions import TimeoutException, NoSuchElementException
 from selenium.webdriver.support import expected_conditions as EC
 
 # [LANG_TC_001] 프로필 메뉴에서 언어 설정 클릭 시 지원 언어 목록이 표시되는지 확인
-def test_language_menu_shows_supported_languages():
+def test_language_menu_shows_supported_languages(logged_in_driver):
     
     #1. 로그인
-    driver = setup_driver(EMAIL, PW)
+    #driver = setup_driver(EMAIL, PW)
     
     try:
         # 1. 사용자 아이콘 클릭
-        personl_con = driver.find_element(By.CSS_SELECTOR, '[data-testid="PersonIcon"]')
+        personl_con = logged_in_driver.find_element(By.CSS_SELECTOR, '[data-testid="PersonIcon"]')
         personl_con.click()
         print('✔️ 사용자 아이콘 클릭 완료')
         time.sleep(1)
 
         # 2. 언어 설정 클릭
         print('== 언어 설정 클릭 중 ==')
-        language_setting = driver.find_element(By.XPATH, "//span[text()='언어 설정']")
+        language_setting = logged_in_driver.find_element(By.XPATH, "//span[text()='언어 설정']")
         language_setting.click()
         print('✔️ 언어 설정 클릭 완료')
         time.sleep(2)
@@ -44,7 +44,7 @@ def test_language_menu_shows_supported_languages():
             current_locator = (By.XPATH, language_time_xpath_template.format(language))
             
             try:
-                driver.find_element(*current_locator)
+                logged_in_driver.find_element(*current_locator)
                 print(f" ✔️ [성공] '{language}' 항목이 확인 되었습니다.")
             except NoSuchElementException:
                 print(f"❎ [실패] '{language}' 항목을 찾을 수 없습니다.")
@@ -52,4 +52,4 @@ def test_language_menu_shows_supported_languages():
         assert test_passed, "모든 언어 항목이 드롭다운에 표시되지 않았습니다."
     
     finally:
-        driver.quit()
+        logged_in_driver.quit()
