cf174fa52858111b3750cf6044617cef80ccb066
eunyoung chae <wooon840824@gmail.com>
2025-12-16 14:16:52 +0900

    문서 파일 업로드 관련 TC 및 테스트 코드 추가 - 헤더가 손상된 이미지 업로드 시 에러 메시지가 표시되는지 확인 - 빈 문서가 정상 업로드되는지 확인 - 암호화 문서 업로드 시 에러 메시지가 표시되는지 확인 - 지원 확장자 문서가 정상 업로드되는지 확인 - 미지원 확장자 문서 업로드 시 에러 메시지가 표시되는지 확인 - 허용 용량 초과 문서 업로드 시 에러 메시지가 표시되는지 확인 - 경계값 용량 문서 업로드 시 정상 업로드되는지 확인 (49MB) / (49.9MB) - 여러 문서 동시 업로드 시 정상 업로드되는지 확인 - 특수문자가 포함된 문서 파일명 업로드 시 정상 업로드 확인 - 헤더가 손상된 문서 업로드 시 에러 메시지가 표시되는지 확인

---
+++ eunyoung_chae/tests/multimodal/test_document_upload_empty_file_succeeds.py
@@ -0,0 +1,48 @@
+import time
+import os
+from src.config.config import *
+from src.utils.helpers import login, setup_driver, logout, get_file_path, open_file_upload_dialog
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.webdriver.support import expected_conditions as EC
+
+
+# 빈 문서가 정상 업로드되는지 확인
+
+def test_document_upload_empty_file_succeeds():
+    
+    # 1. 로그인, driver 객체
+    driver = setup_driver(EMAIL, PW)
+    wait = WebDriverWait(driver, 10)
+    
+    # 2. 파일 업로드 창 열기
+    open_file_upload_dialog(driver)
+    
+    # 3. 문서 파일 경로 지정
+    print("업로드 할 빈 문서 파일 경로")
+    relative_path = '../../src/resources/asserts/files/test_empty.pdf'
+    file_path = get_file_path(relative_path)
+    file_name = os.path.basename(file_path) 
+    
+    # 4. 문서 파일 업로드
+    print("빈 문서 파일 업로드 시도 중")
+    file_input_element = wait.until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, 'input[type="file"]'))
+    )
+    file_input_element.send_keys(file_path)
+    time.sleep(5)
+    
+    # 5. 문서 파일 업로드 확인
+    wait.until(
+        EC.presence_of_element_located((By.XPATH, f"//span[text()='{file_name}']"))
+    )
+    print("파일 카드가 나타남")
+    
+    # 추가 안정화 대기
+    time.sleep(3)  
+    
+    # 테스트 성공 여부 확인
+    uploaded_image = driver.find_element(By.XPATH, f"//span[text()='{file_name}']")
+    assert uploaded_image.is_displayed(), "업로드된 빈 문서 파일 카드가 화면에 나타나지 않았습니다."
+    print("== 빈 문서 파일 업로드 완료 ==")
