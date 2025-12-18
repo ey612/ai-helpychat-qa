11b329bdfa44f4654a781fd1d4cca748316de067
hanseungkyung <hsk1822@gmail.com>
2025-12-19 00:42:32 +0900

    Add exam folder

--- hanseungkyung/constants.py
+++ hanseungkyung/constants.py
@@ -1,5 +1,7 @@
 BASE_URL = "https://qaproject.elice.io/ai-helpy-chat"
 
+ACCOUNT_URL = "https://accounts.elice.io/members/account"
+
 # ⚠️ 여기 값은 "반드시 이미 가입된 이메일"로 바꿔줘야 TC_009가 안정적으로 PASS됨
 DUPLICATE_EMAIL = "g3a3team06@elicer.com"
 
